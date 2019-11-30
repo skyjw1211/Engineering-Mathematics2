f7fb2c6557feffc7c95f09fea39c32ecb36dbd8c
skyjw1211 <47693498+skyjw1211@users.noreply.github.com>
2019-11-30 19:51:45 +0900

    Add files via upload

---
+++ coeff_changer.m
@@ -0,0 +1,23 @@
+function [new_c, new_d] = coeff_changer(c, r, order_r)
+% c =  [-0.5000   -0.1667    0.6667];
+% order_r = [1 1 1];
+% r = [-1 -3 0];
+% is_complex = [0 0 0];
+% 
+% c = [0.5000 + 1.0000i   0.5000 - 1.0000i];
+% order_r = [1     1];
+% is_complex = [1 1];
+% r = [-1.0000 + 1.0000i; -1.0000 - 1.0000i];
+
+new_c = [];
+new_d = [];
+
+for k = 1:length(r)
+    s = [1, -r(k)];
+    for i = 2:order_r(k)
+        s = conv(s, s);
+    end
+    new_d = [new_d; s];
+    new_c = [new_c; c(k)];
+end
+
